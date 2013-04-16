e0b2078fe4af43f3968be93d17a9b3e7ca1b3b75
bspkrs <bspkrs@gmail.com>
2013-04-16 17:14:33 -0400

    fixed version URL and a few small things

--- src/bspkrs/hiddendoors/HiddenDoors.java
+++ src/bspkrs/hiddendoors/HiddenDoors.java
@@ -9,28 +9,35 @@ import bspkrs.util.BSPropRegistry;
 
 public class HiddenDoors
 {
-    public static String modDir            = "hiddendoors";
-    public static int    iOff              = 256;
-    public static Block  door;
-    public static Item   itemDoor;
+    public static String   modDir            = "hiddendoors";
+    public static int      iOff              = 256;
+    public static Block    door;
+    public static Item     itemDoor;
     @BSProp
-    public static int    hiddenDoorBlockID = 1699;
+    public static int      hiddenDoorBlockID = 1699;
     @BSProp
-    public static int    hiddenDoorItemID  = 16099;
+    public static int      hiddenDoorItemID  = 16099;
+    
+    private static boolean initialized       = false;
     
     public static void init()
     {
-        BSPropRegistry.registerPropHandler(HiddenDoors.class);
-        
-        door = (new BlockHiddenDoor(hiddenDoorBlockID)).setHardness(1.2F).setStepSound(Block.soundWoodFootstep).setUnlocalizedName("hiddenDoor");
-        itemDoor = (new ItemHiddenDoor(hiddenDoorItemID)).setUnlocalizedName("itemHiddenDoor");
-        
-        ModLoader.registerBlock(door);
-        ModLoader.addName(itemDoor, "Hidden Door");
-        ModLoader.addRecipe(new ItemStack(itemDoor), new Object[]
+        if (!initialized)
         {
-                "XX", "XX", "XX", Character.valueOf('X'), Block.bookShelf
-        });
+            BSPropRegistry.registerPropHandler(HiddenDoors.class);
+            
+            door = (new BlockHiddenDoor(hiddenDoorBlockID)).setHardness(1.2F).setStepSound(Block.soundWoodFootstep).setUnlocalizedName("hiddenDoor");
+            itemDoor = (new ItemHiddenDoor(hiddenDoorItemID)).setUnlocalizedName("itemHiddenDoor");
+            
+            ModLoader.registerBlock(door);
+            ModLoader.addName(itemDoor, "Hidden Door");
+            ModLoader.addRecipe(new ItemStack(itemDoor), new Object[]
+            {
+                    "XX", "XX", "XX", Character.valueOf('X'), Block.bookShelf
+            });
+            
+            initialized = true;
+        }
     }
     
 }
--- src/net/minecraft/src/mod_HiddenDoors.java
+++ src/net/minecraft/src/mod_HiddenDoors.java
@@ -8,16 +8,11 @@ public class mod_HiddenDoors extends BaseMod
 {
     public static boolean     allowUpdateCheck;
     private ModVersionChecker versionChecker;
-    private String            versionURL = "http://bspk.rs/Minecraft/1.5.1/crystalWing.version";
-    private String            mcfTopic   = "http://www.minecraftforum.net/topic/1009577-";
+    private String            versionURL = "http://bspk.rs/Minecraft/1.5.1/hiddenDoors.version";
+    private String            mcfTopic   = "http://www.minecraftforum.net/topic/1773498-";
     
     public mod_HiddenDoors()
-    {
-        allowUpdateCheck = mod_bspkrsCore.allowUpdateCheck;
-        
-        if (allowUpdateCheck)
-            versionChecker = new ModVersionChecker(getName(), getVersion(), versionURL, mcfTopic);
-    }
+    {}
     
     @Override
     public String getName()
@@ -40,13 +35,16 @@ public class mod_HiddenDoors extends BaseMod
     @Override
     public void load()
     {
+        HiddenDoors.init();
+        
+        allowUpdateCheck = mod_bspkrsCore.allowUpdateCheck;
+        
         if (allowUpdateCheck)
         {
+            versionChecker = new ModVersionChecker(getName(), getVersion(), versionURL, mcfTopic);
             versionChecker.checkVersionWithLogging();
             ModLoader.setInGameHook(this, true, true);
         }
-        
-        HiddenDoors.init();
     }
     
     @Override
